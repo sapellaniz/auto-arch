42cbc9e4e9379005158664d835bf9569e1491e34
Sergio Apellániz <s4nt4t3cl4@protonmail.com>
2020-09-12 16:48:52 +0000

    Add new file

---
+++ autoArch.sh
@@ -0,0 +1,14 @@
+# This script noseke nosekuan
+# Author yop
+
+# Check UID & $HOME
+[ $(id -u) -ne 0 ] && echo "error: you cannot perform this operation unless you are root."
+[ "$HOME" == "/root" ] && echo "warning: you are installing the environment for root user, please run sudo -E bash autoArch.sh or comment line 2"
+
+# Install
+pacman -S --noconfirm xorg xorg-xinit bspwm sxhkd terminator rofi git
+git clone "$MYREPO!!!!!!" 
+mv dotfiles/.* ~/       // .bashrc .xinitrc .config/sxhkd/sxhkdrc .config/sxhkd/sxhkdrc
+
+
+# sudo pacman -S virtualbox-guest-utils && sudo systemctl enable vboxservice && reboot
