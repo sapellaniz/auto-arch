4a73f3b6884b60322fa71bc53e7462f1bbc8b8b6
Sergio Apellániz <s4nt4t3cl4@protonmail.com>
2020-09-13 01:25:52 +0000

    Update autoArch.sh

--- autoArch.sh
+++ autoArch.sh
@@ -1,14 +1,17 @@
 # This script noseke nosekuan
 # Author yop
 
-# Check UID
-[ "$(id -u)" -eq 0 ] && echo "warning: you are installing the environment for root user, if you really want it just comment line X." && exit
+# Check UID & yay
+[ "$(id -u)" -eq 0 ] && echo "error: avoid running autoArch.sh as root/sudo." && exit
+if ! $(which yay &>/dev/null) ; then
+    echo "error: yay not found." && exit
+fi
 
 # Install
 sudo pacman -S --noconfirm xorg xorg-xinit bspwm sxhkd terminator mlocate rofi firefox zsh man
-yay -S --noconfirm zsh-syntax-highlighting-git polybar // NO CONFIRM?
+yay -S --noconfirm zsh-syntax-highlighting-git polybar-git picom-ibhagwan-git // NO CONFIRM?
 usermod -s $(which zsh) $(whoami)
-m
+
 su cp dotfiles/.* ~/       // .zshrc .xinitrc .config/bspwm/bspwmrc .config/sxhkd/sxhkdrc 
 
 
