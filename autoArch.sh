15a1166519d67e0a7cac7c59b16dd6a01536300b
Sergio Apellániz <s4nt4t3cl4@protonmail.com>
2020-09-13 03:03:27 +0000

    Update autoArch.sh

--- autoArch.sh
+++ autoArch.sh
@@ -9,15 +9,23 @@ fi
 
 # Install
 sudo pacman -S --noconfirm xorg xorg-xinit bspwm sxhkd xwallpaper terminator mlocate rofi firefox zsh man alsa-utils pulseaudio
-yay -S --noconfirm zsh-syntax-highlighting-git polybar-git picom-ibhagwan-git ttf-nerd-fonts-hack-complete-git // NO CONFIRM?
-usermod -s $(which zsh) $(whoami)
+yay -S --noconfirm zsh-syntax-highlighting-git polybar-git picom-ibhagwan-git ttf-nerd-fonts-hack-complete-git
+sudo usermod -s $(which zsh) $(whoami)
 
-su cp dotfiles/.* ~/       // .zshrc .xinitrc 
-                            .config/bspwm/bspwmrc  
-                            .config/sxhkd/sxhkdrc
-                            .config/picom/picom.conf
-                            .config/polybar/config
-                            .config/polybar/launch.sh
+cp dotfiles/.* ~/       
+
+#.zshrc
+#.xinitrc
+#.config/wall.png
+#.config/bspwm/bspwmrc  
+#.config/sxhkd/sxhkdrc
+#.config/picom/picom.conf
+#.config/polybar/config
+#.config/polybar/launch.sh
+#.config/bin/ethernet_status.sh
+#.config/bin/rofi-power.sh
 
 rm ~/.bash*
+
 # sudo pacman -S virtualbox-guest-utils && sudo systemctl enable vboxservice && reboot
+# TODO: 1- Reflector ; 2- Orphans
