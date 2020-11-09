f94d9ddaa194b95b2aca605550a3adc5115375f7
sapellaniz <sapellaniz@protonmail.com>
2020-11-10 00:35:10 +0100

    Script funcional y actualización del README.md

--- README.md
+++ README.md
@@ -24,10 +24,18 @@ bash autoArch.sh [ARM]
 - **Atajos:**
 
 - **Dotfiles:**
+    - ~/.config/
+    - ~/.config/
+    - ~/.config/
+    - ~/.config/
+    - ~/.config/
+    - ~/.config/
+    - ~/.config/
+    - ~/.config/wall.png
 
 - **Silent boot:**
 
-Salta el grub, esconde la salida de systemd, un usuario específio se auto loguea en la tty1 y se redirige la stdout de startx a /dev/null.
+Salta el grub, esconde la salida de systemd, un usuario específio se auto loguea en la tty1, se redirige la stdout de startx a /dev/null y se elimina el banner.
 ```
 # grub (solo x86)
 /etc/default/grub
@@ -47,5 +55,32 @@ systemctl edit getty@tty1.service
 # startx
 vim .zprofile
     [ "$(tty)" = "/dev/tty1" ] && ! ps -e | grep -qw Xorg && exec startx &> /dev/null
+
+# banner
+rm /etc/motd
 ```
 
+- **Raspberry tips:**
+
+Instalar el sistema en un USB 3.0 en vez de en una memoria micro SD, aumenta notablemente el rendimiento.
+
+1. Arrancar la rasp desde USB:
+
+    - Seguir el siguiente tutorial hasta el paso 13 (no clonar SD)
+    - https://www.tomshardware.com/how-to/boot-raspberry-pi-4-usb
+
+2. Instalar Arch ARM para raspberry en un USB:
+
+    - Seguir la guía oficial:
+    - https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-4
+    - Antes de desmontar las particiones ejecutar los siguientes comandos:
+
+```
+# Cambiar sda por el disco correspondiente
+(sed -i 's/mmcblk0p1/sda1/g' root/etc/fstab)
+(sed -i 's/mmcblk0p2/sda2/g' boot/cmdline.txt)
+```
+
+3. Deshabilitar molestos mensajes del sistema:
+
+    - Añadir "audit=0" a la línea de "/boot/cmdline.txt"
--- autoArch.sh
+++ autoArch.sh
@@ -1,43 +1,3 @@
-# Boot raspberry pi from USB & install arch on USB
-	https://www.tomshardware.com/how-to/boot-raspberry-pi-4-usb
-
-	# Prepare usb
-	#	Before unmounting the partitions
-	#	(sed -i 's/mmcblk0p1/sda1/g' root/etc/fstab)
-	#	(sed -i 's/mmcblk0p2/sda2/g' boot/cmdline.txt)
-	https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-4
-
-# Basic config
-	# Disable anoying audit messages
-	(append audit=0 to /boot/cmdline.txt)
-
-	pacman -Syu
-	pacman -S sudo vim git
-
-	echo KEYMAP=es > /etc/vconsole.conf
-	userdel -r alarm
-	passwd
-	useradd x -m -G wheel,storage,power
-	passwd x
-	# uncomment %wheel ALL=(ALL) ALL
-	ln -s /usr/bin/vim /usr/bin/vi
-	visudo
-	su x
-	
-	# yay
-	cd /opt
-	sudo git clone https://aur.archlinux.org/yay-git.git
-	sudo chown -R x:x yay-git
-	cd yay-git
-	makepkg -si
-	# guess aditions
-	# sudo pacman -S virtualbox-guest-utils	
-	# config -> pantall -> controlador grafico -> VBoxVGA
-	
-###
-# Auto-arch.sh
-#####
-
 # Script for build a cool graphical user interface for Arch Linux
 # Author Sergio Apellaniz
 
@@ -47,71 +7,54 @@ if ! $(which yay &>/dev/null) ; then
     echo "error: yay not found." && exit
 fi
 
+# Select best mirrors before start downloading
+sudo pacman -S reflector
+sudo reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+
 # DWM & DMENU & X-Server & xwallpaper
-	sudo pacman -S --noconfirm base-devel binutils fakeroot make automake autoconf xorg /xorg-server /xf86-video-fbdev xorg-xinit xwallpaper
-	yay -S --noconfirm ttf-nerd-fonts-hack-complete-git
+mkdir ~/.config
+sudo pacman -S --noconfirm base-devel binutils fakeroot make automake autoconf xorg /xorg-server /xf86-video-fbdev xorg-xinit xwallpaper
+yay -S --noconfirm ttf-nerd-fonts-hack-complete-git
 
-	cd /usr/src
-	sudo git clone git://git.suckless.org/dwm
-	sudo git clone git://git.suckless.org/dmenu
-	sudo git clone git://git.suckless.org/st
-	cd dwm
-	sudo make clean install
-	cd ../dmenu
-	sudo make clean install
-	cd ../st
-	sudo make clean install
+pushd ~/.config
+git clone git://git.suckless.org/dwm
+git clone git://git.suckless.org/dmenu
+git clone git://git.suckless.org/st
+popd
+mv -f dotfiles/config.h ~/.config/dwm
+pushd ~/.config/dwm
+make clean install
+cd ../dmenu
+make clean install
+cd ../st
+make clean install
 
-	echo -e "setxkbmap es\nxwallpaper --focus ~/.config/wall.png\nexec dwm" > ~/.xinitrc
+echo -e "setxkbmap es\nxwallpaper --focus ~/.config/wall.png\nexec dwm" > ~/.xinitrc
 
 
 # zsh & tmux
-	mkdir -p ~/.config/zsh ~/.config/tmux ~/.config/vim
+mkdir ~/.config/zsh ~/.config/tmux ~/.config/vim
+popd
+mv dotfiles/.z* ~/.config/zsh
+mv dotfiles/tmux.conf ~/.config/tmux
+mv dotfiles/wall.png ~/.config
 
-	sudo pacman -S --noconfirm zsh tmux zsh-syntax-highlighting
+sudo pacman -S --noconfirm zsh tmux zsh-syntax-highlighting
 
-	sudo usermod -s $(which zsh) $(whoami)
+sudo usermod -s $(which zsh) $(whoami)
 
-	echo "es_ES.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
-	echo "LC_ALL=es_ES.UTF-8" | sudo tee -a /etc/environment
-	echo "LANG=es_ES.UTF-8" | sudo tee -a /etc/locale.conf
-	sudo locale-gen es_ES.UTF-8
+echo "es_ES.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+echo "LC_ALL=es_ES.UTF-8" | sudo tee -a /etc/environment
+echo "LANG=es_ES.UTF-8" | sudo tee -a /etc/locale.conf
+sudo locale-gen es_ES.UTF-8
 
-	echo "$(which tmux) -f ~/.configm/tmux/tmux.conf" | sudo tee -a /usr/local/bin/tmux
-	echo "$(which vim) -u ~/.config/vim/vimrc" | sudo tee -a /usr/local/bin/vim
-	ln -s ~/.config/zsh/.zshenv ~/.zshenv
-
-# Silent boot
-	# systemd
-		echo "quiet" >> /boot/cmdline.txt
-
-	# agetty
-	systemctl edit getty@tty1.service
-        	[Service]
-	        ExecStart=
-	        ExecStart=-/usr/bin/agetty --skip-login --nonewline --noissue --autologin username --noclear %I $TERM
-
-	# motd
-	sudo rm /etc/motd
-
+echo "$(which tmux) -f ~/.configm/tmux/tmux.conf" | sudo tee -a /usr/local/bin/tmux
+echo "$(which vim) -u ~/.config/vim/vimrc" | sudo tee -a /usr/local/bin/vim
+ln -s ~/.config/zsh/.zshenv ~/.zshenv
 
 # Remove orphans
 sudo pacman -Rs $(pacman -Qqtd) --noconfirm
  
 # Auto clean
-rm -fr ../.git
-rm -fr $PWD
-
-# DOTFILES
-	/usr/src/
-		dwm
-		dmenu
-		st
-	~/.config/zsh/.zshenv
-	~/.config/zsh/.zshrc
-	~/.config/zsh/.zprofile
-	~/.config/tmux/tmux.conf
-	~/.config/vim/vimrc
-	~/.config/wall.png
-
-https://feldspaten.org/2020/07/05/raspberry-pi-4-and-kvm/
+cd ..
+rm -fr auto-arch
